753c771f1edd0f252cfaa3eaadc9d2090263435a
maciejeicam <maciej_michalik@onet.pl>
2023-03-02 13:50:39 +0100

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Maciej Michalik on 02/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
